3e3f36eaa3bad4f7fc7b31f88caab2b959271c6e
Chandravarman19 <Chandravarman12@gmail.com>
2025-09-19 12:35:15 +0530

    Added save and Viewing Functions

--- TaskManager.py
+++ TaskManager.py
@@ -9,3 +9,16 @@ def load_tasks(): #Load tasks from file
         return[]
     except json.JSONDecodeError: #If file is corrupted?empty, return empty list.
         return[]
+    
+def save_tasks(): # Save tasks to file
+    with open(FILENAME, "w") as f:
+        json.dump(tasks, f, indent=4)
+
+def views_tasks(): # Display all tasks
+    if not tasks:
+        print("\n No tasks found!\n")
+        return
+    print("\n Your tasks:")
+    for i, task in enumerate (tasks, start = 1):
+        print(f"{i}.{task['title']}-{task['status']}")
+    print()
